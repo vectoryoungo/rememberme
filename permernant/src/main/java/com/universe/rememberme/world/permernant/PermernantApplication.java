2a4b6f5a128562785711cbd0df8f801f31da65e6
居洪涛 <hongtao.ju@blockchain001.com>
2019-04-25 10:44:15 +0800

    initial project

---
+++ permernant/src/main/java/com/universe/rememberme/world/permernant/PermernantApplication.java
@@ -0,0 +1,13 @@
+package com.universe.rememberme.world.permernant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermernantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermernantApplication.class, args);
+	}
+
+}
